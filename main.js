abc422dc3d4f108de9bfdea92ebfbcde4d2265de
Renee Doll <renee@reneedoll.com>
2022-10-07 10:00:45 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const quarterValue = (scaryNumber) => {
+const quarter = scaryNumber / 4
+
+return quarter
+}
+
+const returnedNumber = quarterValue(56)
+console.log(returnedNumber)
+
+// Invoke the function and store the return value
+
+
+// Log the return value
+console.log()
